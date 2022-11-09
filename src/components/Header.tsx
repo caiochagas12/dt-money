539386baf33738312b55e9045c62b76ab586ec94
caiochagas12 <caiochagas77@hotmail.com>
2022-11-09 10:35:06 -0300

    Header done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,14 @@
+import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import logoImg from "../../assets/logo.svg";
+
 export function Header () {
-    return(
-        <div>Header</div>
-    )
-}
+    return (
+        <HeaderContainer>
+          <HeaderContent>
+            <img src={logoImg} alt="" />
+    
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </HeaderContent>
+        </HeaderContainer>
+      );
+    }
