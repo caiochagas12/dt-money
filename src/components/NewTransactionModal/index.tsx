e490dcbf24c10df5846f780a06106fe97fe4b248
caiochagas12 <caiochagas77@hotmail.com>
2022-11-15 14:21:02 -0300

    creating new transaction fuction

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,9 +1,10 @@
-import { zodResolver } from '@hookform/resolvers/zod';
-
+import { useContext } from 'react';
 import { Controller, useForm } from 'react-hook-form';
-import * as z from 'zod';
 
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as z from 'zod';
 import * as Dialog from '@radix-ui/react-dialog';
+import { TransactionsContext } from '../../contexts/TransactionsContext';
 
 import { X, ArrowCircleUp, ArrowCircleDown } from "phosphor-react";
 
@@ -20,12 +21,15 @@ const newTransactionFormSchema = z.object({
 type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>;
 
 export function NewTransactionModal() {
+  
+  const { createTransaction } = useContext(TransactionsContext);
 
   const {
     control,
     register,
     handleSubmit,
-    formState: { isSubmitting }
+    formState: { isSubmitting },
+    reset
   } = useForm<NewTransactionFormInputs>({
     resolver: zodResolver(newTransactionFormSchema),
     defaultValues:{
@@ -34,9 +38,17 @@ export function NewTransactionModal() {
   })
 
   async function handleCreateNewTransaction(data: NewTransactionFormInputs) {
-    await new Promise(resolve => setTimeout(resolve, 2000));
 
-    console.log(data);
+    const { description, price, category, type } = data;
+
+    await createTransaction({
+      description,
+      price,
+      category,
+      type,
+    });
+
+    reset();
   }
 
   return (
